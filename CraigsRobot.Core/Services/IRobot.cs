575f30cb9c4a8427e30042f9ee31edfa2b867b45
Craig J Hannon <c.j.hannon@gmail.com>
2021-08-04 19:31:54 +0100

    added in initial project setup

---
+++ CraigsRobot.Core/Services/IRobot.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CraigsRobot.Core.Services
+{
+    public interface IRobot
+    {
+        Task WakeUp(CancellationToken cancellationToken);
+    }
+}
---
+++ CraigsRobot.Infrastructure/Services/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CraigsRobot.Core.Services;
+
+namespace CraigsRobot.Infrastructure.Services
+{
+    public class Robot :IRobot
+    {
+        public Task WakeUp(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"{DateTime.Now}");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
