30d3cd413f05899d354875a722f12536efb752cc
Craig J Hannon <c.j.hannon@gmail.com>
2021-08-04 19:11:51 +0100

    initial commit

---
+++ CraigsRobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CraigsRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
